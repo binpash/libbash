81ba6841a4097dab87bee9a820ad8194afcd5891
sethsabar <84099796+sethsabar@users.noreply.github.com>
2023-12-01 15:35:05 -0500

    Update test.py

--- libbash/test/test.py
+++ libbash/test/test.py
@@ -1,6 +1,5 @@
-from libbash import bash_to_ast, ast_to_bash, ast_to_json
+from ..api import bash_to_ast, ast_to_bash, ast_to_json
 import os
-import pytest
 
 BASH_FILE_PATH = os.path.join(os.path.dirname(
     __file__), "..", "..", "bash-5.2", "bash.so")
@@ -28,8 +27,33 @@ def test_bash_to_ast_equality():
     ast_to_bash and the back via bash_to_ast. The resulting AST is compared to the
     original AST and if they are not equal, the test fails.
     """
+    TMP_DIR = "/tmp/libbash"
+    TMP_FILE = "/tmp/libbash/test.sh"
+
+    # make a temporary directory to store the bash files
+    os.system(f"rm -rf {TMP_DIR}")
+    os.system(f"mkdir {TMP_DIR}")
     for test_file in get_test_files():
-        ast = bash_to_ast(test_file)
+        try:
+            ast = bash_to_ast(test_file)
+            print(test_file)
+        except Exception as e:
+            continue
         bash = ast_to_bash(ast)
-        ast2 = bash_to_ast(bash)
+        os.system(f"echo '{bash}' > {TMP_FILE}")
+        ast2 = bash_to_ast(f"{TMP_FILE}")
+        os.system(f"rm {TMP_FILE}")
         assert ast == ast2
+
+    os.system(f"rm -rf {TMP_DIR}")
+
+
+def run_tests():
+    """
+    Runs all the tests in this file
+    """
+    print("Running tests...")
+    test_bash_to_ast_equality()
+    print("bash_to_ast equality test passed!")
+    print("All tests passed!")
+    
