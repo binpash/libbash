d05a5e678d55dba3f724402d9b9ff5aef7c8c428
sethsabar <84099796+sethsabar@users.noreply.github.com>
2023-12-01 15:27:30 -0500

    Add list equality function

---
+++ libbash/bash_command/util.py
@@ -0,0 +1,22 @@
+
+def list_same_elements(l1: list, l2: list) -> bool:
+    """
+    Checks if two lists are equal (order doesn't matter)
+    :param l1: The first list
+    :param l2: The second list
+    :return: True if the lists are equal, false otherwise
+    """
+    if len(l1) != len(l2):
+        return False
+    
+    for i in l1:
+        found_match = False
+        for j in l2:
+            if i == j:
+                l2.remove(j)
+                found_match = True
+                break
+        if not found_match:
+            return False
+    
+    return True
