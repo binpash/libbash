33cfd465078a0fe15ab04ae1a9d3d749ac8e4825
sethsabar <sethsabar@gmail.com>
2024-04-15 02:30:36 +0000

    some maybe fixes to setup

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ class build_libbash(build_py):
 setup(name='libbash',
       packages=['libbash', 'libbash.bash_command'],
       cmdclass={'build_py': build_libbash},
-      package_data={'libbash': ['bash-5.2/*']},
-      version='0.1.9',
+      package_data={'libbash':['libbash/bash-5.2']},
+      version='0.1.10',
       description="A Python library for parsing Bash scripts into an AST",
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
@@ -49,6 +49,5 @@ setup(name='libbash',
       author='Seth Sabar',
       author_email='sethsabar@gmail.com',
       license="GPL-3.0",
-      include_package_data=True,
       has_ext_modules=lambda: True)
 
